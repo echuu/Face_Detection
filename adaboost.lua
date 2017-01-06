cd1aee26432553e6e02098a6372fcb11866c99d4
echuu <chuu3ric@gmail.com>
2017-01-05 21:20:51 -0800

    adabosot

--- adaboost.lua
+++ adaboost.lua
@@ -44,9 +44,11 @@ local classify = require "classifly.lua"; -- access kmeans() function
 
 boost.adaboost()
 
-local function adaboost()
+local function adaboost(T)
 	-- precompute the projections
 
+
+	start_time = os.time();
 
 	delta        = torch.load('delta.dat');
 	proj         = torch.load('projections.dat');
@@ -56,54 +58,87 @@ local function adaboost()
 	nonface_sd   = torch.load('nonface_sd.dat');
 	Y_train      = torch.load('Y_train.dat');
 
-	num_imgs     = proj:size()[1]; -- num of total images (# rows of proj)
+	end_time = os.time();
+	elapsed_time = os.difftime(end_time, start_time);
+	print('Finished reading in data files. Total time: '..elapsed_time);
 
+	num_imgs     = proj:size()[1]; -- num of total images (# rows of proj)
+	err_mat      = torch.Tensor(num_imgs, proj:size()[2]);
+
+	-- precompute the classifications
+	start_time = os.time();
+	for i = 1, proj:size()[2] do
+		-- pass in i-th column of proj to classiifer
+		-- class: +1 if correct class vector holds correct class., else -1
+		class = classify.ll_classify(proj[{{}, {i}}],
+			face_mean[i], face_sd[i], nonface_mean[i], nonface_sd[i]);
+		
+		-- # classified incorrectly = sum(err_indicator)
+		err_indicator = torch.ne(Y_train, class); -- num_imgs x delta_size
+
+		-- error     = indicator:sum() / num_imgs; -- STORE THIS FOR USE IN ADA
+		-- print('iter '..i..' classifcation error: '..error);
+
+		err_mat[{{}, {i}}] = err_indicator;
+	end
+	
+	end_time = os.time();
+	elapsed_time = os.difftime(end_time, start_time);
+	print('Finished classifications. Total time: '..elapsed_time);
+
+
+	---- BOOSTING STEP ---------------------------------------------------------
 
 	-- initialize current weights, previous weights for images
-	wts_cur  = torch.Tensor(num_imgs, 1);
-	wts_prev = torch.Tensor(num_imgs, 1);
+	wts_cur  = torch.Tensor(1, num_imgs);
+	wts_prev = torch.Tensor(1, num_imgs);
 
 	wts_cur  = 1 / num_imgs;
 	wts_prev = 1 / num_imgs;
 
 
-	-- precompute the classifications
-		-- make call to classify.kmeans()
-	print('Begin classification');
+	print('Begin adaboost');
 	start_time = os.time();
 
-	weighted_err = torch.Tensor(delta_size, 1);
-	for i = 1, proj:size()[2] do
-		-- pass in i-th column of proj to classiifer
-		class = classify.ll_classify(proj[{{}, {i}}],
-			face_mean[i], face_sd[i], nonface_mean[i], nonface_sd[i]);
-		
-		-- indicator: +1 if correct class vector holds correct class., else -1
-		indicator = torch.eq(Y_train, class);
+	weighted_err = torch.Tensor(1, delta_size);
+	alpha        = torch.Tensor(1, T);
+	ada_index    = torch.Tensor(1, T);
+	F_ip         = torch.Tensor(num_imgs, 1); -- store the inner product of all
+											  -- imgs with every weak classifier
 
-		-- store weighted error
-		-- sorted ratio
+	for t = 1, T do
+		iter_start = os.time();
 
-	end
+		-- calculate weighted error
+		wt_err = wts_cur * err_mat; -- 1 x delta_size, wt_error correspond
+									      -- to each weak classifier
+
+		-- find weighted classifier with min. weighted error
+		min_err, min_ind = torch.min(wt_err, 2); -- 2 b/c weighted_err is
+											     -- row vector
+
+		ada_index[t] = min_ind;
+
+		-- update wts_prev, alpha (alpha)
+		wts_prev = wts_cur;
+		alpha(t) = 0.5 * torch.log((1 - wt_err) / wt_err);
+
+		-- calculate empirical error (minimize)
 
 
+		------------------------ end calculations ------------------------------
+		end_iter_time = os.time();
+		elapsed_time = os.difftime(end_iter_time, iter_start);
+		print('iter '..i.. ' -- '..elapsed_time ' seconds');
+	end ------------------------ end adaboost iteration ------------------------
+	
 	end_time = os.time();
 	elapsed_time = os.difftime(end_time, start_time);
 	print('total runtime of classifications: ' .. elapsed_time .. 'seconds');
+	print('End adaboost');
 
+	---- END BOOSTING STEP -----------------------------------------------------
 	
-	
-	-- boost weak classifiers
-
-
-	print('in adaboost');
-
-end
-
-local function computeClassifications()
-
-	print('in computeClassifications');
-
 end
 
 
