44a7fa9d0e06bab8568359c7b2eaf029193373e3
echuu <chuu3ric@gmail.com>
2017-03-30 11:49:27 -0700

    common defintions header file

--- boost.lua
+++ boost.lua
@@ -1,21 +1,14 @@
 local ld  = require('load_data');
 local ext = require('externalFunctions');
-
-local subset_faces    = 800;
-local subset_nonfaces = 3200;
-local dim             = 16;
-local delta_size      = 36480;
-local total_imgs      = subset_faces + subset_nonfaces;
-
-pathname = "/home/eric/data_files/";
+local g   = require('common_defs')
 
 -- faces, nonfaces stored as rows
-faces    = ld.importFaces(pathname, subset_faces);       --  800 x 256
-nonfaces = ld.importNonfaces(pathname, subset_nonfaces); -- 3200 x 256
+faces    = ld.importFaces(g.pathname, g.subset_faces);       --  800 x 256
+nonfaces = ld.importNonfaces(g.pathname, g.subset_nonfaces); -- 3200 x 256
 
 -- generate weak classifiers
 -- each weak classifiers stored as a column vector
-delta = ext.generateWC(dim, delta_size);
+delta = ext.generateWC(g.dim, g.delta_size);
 
 
 -- calculate threshold
@@ -27,5 +20,5 @@ delta = ext.generateWC(dim, delta_size);
 	proj         (total_imgs x delta_size) -- used for classification
 --]]
 face_mean, face_sd, nonface_mean, nonface_sd, proj = ext.calcThreshold(delta,
-	delta_size, faces, nonfaces);
+	g.delta_size, faces, nonfaces);
 
---
+++ common_defs.lua
@@ -0,0 +1,14 @@
+--[[
+	 Common variable definitions used
+--]]
+
+local g = {}
+
+g.subset_faces    = 800;
+g.subset_nonfaces = 3200;
+g.dim             = 16;
+g.delta_size      = 36480;
+g.total_imgs      = g.subset_faces + g.subset_nonfaces;
+g.pathname        = "/home/eric/data_files/";
+
+return g;
